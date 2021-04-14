f9f01882684c4fe419fcb202fa93f22a7e4ec5f1
Automattic <themes@wordpress.com>
2021-04-14 16:41:05 +0000

    a93c71cd00010e147280f672ed1138c496bfa78144f9ea1b3caf371d885d538a

---
+++ js/sidebar.js
@@ -0,0 +1,32 @@
+/**
+ * sidebar.js
+ *
+ * Handles resizing the content area.
+ */
+( function() {
+
+	var content, sidebar;
+	content = document.getElementById( 'content' );
+	sidebar = document.getElementById( 'secondary' );
+
+	if ( ! sidebar ) {
+		return;
+	}
+
+	function contentMinHeight() {
+
+		if ( window.innerWidth >= 1272 ) {
+			content.style.minHeight = sidebar.offsetHeight + 24 + 'px';
+		} else {
+			content.removeAttribute( 'style' );
+		}
+
+	}
+
+	contentMinHeight();
+
+	setTimeout( contentMinHeight, 1500 );
+
+	window.addEventListener( 'resize', contentMinHeight );
+
+} )();
